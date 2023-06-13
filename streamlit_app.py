5743c3913d15617920854eafdf55b337699e765b
BhanuRohith-Bobby <85399102+BhanuRohith-Bobby@users.noreply.github.com>
2023-06-13 17:42:41 +0530

    Update streamlit_app.py
    added menua and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('idli')
-streamlit.text('dosa')
-streamlit.text('poori')
-
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
